2dc21127f16df6337a0549e9504b0ba9697bb2df
swati kadam <swatikadam169@gmail.com>
2022-04-20 15:51:15 +0530

    first commit

---
+++ BookAppointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace hospital
+{
+    public partial class BookAppointment
+    {
+        public int Id { get; set; }
+        public short AppintmentId { get; set; }
+        public string Service { get; set; }
+        public DateTime? AppointmentDate { get; set; }
+        public string AppointmentMesaage { get; set; }
+        public TimeSpan? AppoinmentTime { get; set; }
+
+        public virtual PatientRegister Appintment { get; set; }
+    }
+}
